c624bbe66fcd81b0a50a14370a23130f76e276a1
truongtrieu123 <uerittruong@gmail.com>
2021-06-15 14:36:25 +0700

    init DAO

---
+++ LibraryManagement/LibraryManagement/Models/DAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    class DAO
+    {
+        public LibraryMangementEntities Database;
+        
+        public DAO()
+        {
+            Database = new LibraryMangementEntities();
+        }
+
+
+    }
+}
