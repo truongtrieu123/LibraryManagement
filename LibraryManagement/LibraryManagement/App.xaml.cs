10cc5f81ab08dfae38c9d43341f09d49b1977bcf
truongtrieu123 <uerittruong@gmail.com>
2021-06-22 20:43:07 +0700

    Update App.xaml.cs

--- LibraryManagement/LibraryManagement/App.xaml.cs
+++ LibraryManagement/LibraryManagement/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +9,18 @@ using System.Windows;
 
 namespace LibraryManagement
 {
+    
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public  DAO AppDAO;
+        public static Views.BooksList BooksListPage;
+        public App()
+        {
+            AppDAO = new DAO();
+        }
+
     }
 }
